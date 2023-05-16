ac61daa946cd9bbd90be3027e8ef2591f95cdb3d
deswaq789 <deswaq789@naver.com>
2023-05-16 19:15:39 +0900

    수주등록,출하,수주관리 하다말음 스크롤 넣어야함

--- src/main/java/mes/smartmes/Controller/mainPageController.java
+++ src/main/java/mes/smartmes/Controller/mainPageController.java
@@ -8,27 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/")
-public class CurrentController {
+public class mainPageController {
 
     @Autowired
     private CalendarService calendarService;
 
-    @GetMapping("/index")
-    public String index(){
+    @GetMapping("/index") //첫페이지
+    public String indexpage(){
         return "index";
     }
 
 
     @RequestMapping //기본 페이지 표시
     public String viewCalendar(){
-        return "/currentPage";
+        return "currentPage";
     }
 
     @GetMapping("/event") //ajax 데이터 전송 URL
@@ -36,6 +34,10 @@ public class CurrentController {
         return calendarService.getEventList();
     }
 
+    @GetMapping("/order")
+    public String orderPage(){
+        return "order";
+    }
 
 
 }
