c294c8aafd0fc0eb74ad5fd2b2b053853758afdc
hyunhwaa <131943851+hyunhwaa@users.noreply.github.com>
2023-05-26 14:59:23 +0900

    경원,현화 합침

--- src/main/resources/js/Scroll.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', adjustSidebarHeight);
-window.addEventListener('resize', adjustSidebarHeight);
-
-function adjustSidebarHeight() {
-    const sidebar = document.getElementById('sidebar');
-    const pageHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-    sidebar.style.height = `${pageHeight}px`;
-}
